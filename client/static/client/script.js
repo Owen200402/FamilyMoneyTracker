bb3cb9a42dfdc7358a92e01cb56ff724e45eb395
Owen Zheng <johndoe@example.com>
2023-08-23 10:47:52 -0700

    Add Submit Error message

--- client/static/client/script.js
+++ client/static/client/script.js
@@ -1,28 +1,30 @@
-// Constants
-const family_register = document.querySelectorAll(".btn-family-register");
-const user_register = document.querySelectorAll(".btn-user-register");
-const user_login = document.querySelectorAll(".btn-user-login");
-
-const user_login_btn = document.querySelector(".btn-user-login");
-const user_signup_btn = document.querySelector(".btn-user-signup");
-const family_signup_btn = document.querySelector(".btn-family-signup");
-
-const user_login_close = document.querySelector(".user-login-close");
-const user_signup_close = document.querySelector(".user-signup-close");
-const family_signup_close = document.querySelector(".family-signup-close");
-
-const username_register = document.querySelector("#username-register");
-const password_register = document.querySelector("#password-register");
-const email_register = document.querySelector("#email-register");
-const first_name_register = document.querySelector("#first-name-register");
-const last_name_register = document.querySelector("#last-name-register");
-
-const user_login_form = document.querySelector("#form-login-user");
-const user_signup_form = document.querySelector("#form-signup-user");
-const family_signup_form = document.querySelector("#form-signup-family");
-
-// Cover Page Linking
 document.addEventListener("DOMContentLoaded", function () {
+  // Constants
+  const family_register = document.querySelectorAll(".btn-family-register");
+  const user_register = document.querySelectorAll(".btn-user-register");
+  const user_login = document.querySelectorAll(".btn-user-login");
+
+  const user_login_btn = document.querySelector(".btn-user-login");
+  const user_signup_btn = document.querySelector(".btn-user-signup");
+  const family_signup_btn = document.querySelector(".btn-family-signup");
+
+  const user_login_close = document.querySelector(".user-login-close");
+  const user_signup_close = document.querySelector(".user-signup-close");
+  const family_signup_close = document.querySelector(".family-signup-close");
+
+  const username_register = document.querySelector("#username-register");
+  const password_register = document.querySelector("#password-register");
+  const email_register = document.querySelector("#email-register");
+  const first_name_register = document.querySelector("#first-name-register");
+  const last_name_register = document.querySelector("#last-name-register");
+
+  const user_login_form = document.querySelector("#form-login-user");
+  const user_signup_form = document.querySelector("#form-signup-user");
+  const family_signup_form = document.querySelector("#form-signup-family");
+
+  const user_validation = document.querySelector("#user-validation");
+
+  // Cover Page Linking
   family_register.forEach((button) => {
     button.addEventListener("click", function (e) {
       closeAllModals();
@@ -41,35 +43,37 @@ document.addEventListener("DOMContentLoaded", function () {
       user_login_btn.click();
     });
   });
-});
 
-// User Creation POST Request
-async function userCreationPOST(event) {
-  event.preventDefault();
-  const form = event.target;
-  const formData = new FormData(form);
+  // User Creation POST Request
+  async function userCreationPOST(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
 
-  try {
-    const response = await fetch("../auth/users/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(Object.fromEntries(formData)),
-    });
+    try {
+      const response = await fetch("../auth/users/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(formData)),
+      });
 
-    const responseData = await response.json();
-    console.log("User created: ", responseData);
-  } catch (error) {
-    console.error("Error: ", error);
+      const responseData = await response.json();
+      console.log("User created: ", responseData);
+    } catch (error) {
+      user_validation.textContent = Object.values(error).flat();
+      console.error("Error: ", error);
+    }
   }
-}
 
-user_signup_form.addEventListener("submit", userCreationPOST);
+  // Submit Event
+  user_signup_form.addEventListener("submit", userCreationPOST);
 
-// Hide All Modals for Front page
-function closeAllModals() {
-  user_login_close.click();
-  user_signup_close.click();
-  family_signup_close.click();
-}
+  // Hide All Modals for Front page
+  function closeAllModals() {
+    user_login_close.click();
+    user_signup_close.click();
+    family_signup_close.click();
+  }
+});
