f85a7f94af9eb2b5626ff264d5e5e4fa87ed3810
Owen Zheng <johndoe@example.com>
2023-08-24 16:02:06 -0700

    Debug SMTP

--- tracker/signals/handlers.py
+++ tracker/signals/handlers.py
@@ -1,24 +1,24 @@
 from django.conf import settings
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, BadHeaderError
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from tracker.models import Member
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_member_for_new_user(sender, instance, created, **kwargs):
-    if created:
-        Member.objects.create(user=instance)
+def create_member_for_new_user(sender, **kwargs):
+    if kwargs["created"]:
+        Member.objects.create(user=kwargs["instance"])
 
-        recipient_name = instance.username
-        email = instance.email
+        # recipient_name = kwargs["instance"].username
+        # email = kwargs["instance"].email
 
-        subject = 'Welcome to MoneyTracker'
-        message = f'Hello {recipient_name},\n\nThank you for signing up your account. We are glad to help you with your money save journey.\n Let us know your questions by calling 778-512-4998. Our service team is glad to help you! \n\nSincerely,\nMoneyTracker Development Team'
-        from_email = 'zcc2280411284@gmail.com'
-        recipient_list = [email]
+        # subject = 'Welcome to MoneyTracker'
+        # message = f'Hello {recipient_name},\n\nThank you for signing up your account. We are glad to help you with your money save journey.\n Let us know your questions by calling 778-512-4998. Our service team is glad to help you! \n\nSincerely,\nMoneyTracker Development Team'
+        # from_email = 'zcc2280411284@gmail.com'
+        # recipient_list = [email]
 
-        try:
-            send_mail(subject, message, from_email, recipient_list)
-        except BadHeaderError:
-            pass
+        # try:
+        #     send_mail(subject, message, from_email, recipient_list)
+        # except BadHeaderError:
+        #     pass
