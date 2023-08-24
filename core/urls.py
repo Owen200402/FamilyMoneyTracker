bc00f4e63231f57468e32e99fa8f251b4ad44d12
Owen Zheng <johndoe@example.com>
2023-08-24 15:29:43 -0700

    Solve Conf Erroor

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,5 @@
 from django.views.generic import TemplateView
 from django.urls import path
-from .import views
-from core.views import email
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='core/index.html'))
--- core/views.py
+++ core/views.py
@@ -1,11 +1,1 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
 from django.shortcuts import render
-
-
-def email(request):
-    try:
-        send_mail('subject', 'message',
-                  'zcc2280411284@gmail.com', ['owen@gmail.com'])
-    except BadHeaderError:
-        pass
-    return render(request, 'hello.html', {'name': 'Mosh'})
