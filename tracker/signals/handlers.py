d021e4cf3b37f7e8cbeed61481b80e00adfb04e2
Owen Zheng <johndoe@example.com>
2023-08-24 16:11:50 -0700

    .

--- tracker/signals/handlers.py
+++ tracker/signals/handlers.py
@@ -15,7 +15,10 @@ def create_member_for_new_user(sender, **kwargs):
 
         subject = 'Welcome to MoneyTracker'
         message = f'Hello {recipient_name},\n\nThank you for signing up your account. We are glad to help you with your money save journey.\n Let us know your questions by calling 778-512-4998. Our service team is glad to help you! \n\nSincerely,\nMoneyTracker Development Team'
-        from_email = 'zcc2280411284@gmail.com'
+        # from_email = 'zcc2280411284@gmail.com'
         recipient_list = [email]
 
-        send_mail(subject, message, from_email, recipient_list)
+        try:
+            send_mail(subject, message, recipient_list)
+        except BadHeaderError:
+            pass
