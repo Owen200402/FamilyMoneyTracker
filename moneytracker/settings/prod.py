fe99bc535de79d2854fb27f7d93d83d4e6bcba8f
Owen Zheng <johndoe@example.com>
2023-08-26 21:08:52 -0700

    set dev.py to gitignore and env var for prod.py

--- moneytracker/settings/prod.py
+++ moneytracker/settings/prod.py
@@ -15,7 +15,7 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zcc2280411284@gmail.com'
-EMAIL_HOST_PASSWORD = 'hpeseooouwprbged'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
