c376147fb82cb09d741de85bd8ebf7addce53e6c
Owen Zheng <johndoe@example.com>
2023-08-30 11:11:41 -0700

    Add Amazon S3 Media and Static server

--- moneytracker/settings/common.py
+++ moneytracker/settings/common.py
@@ -106,8 +106,10 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'http://127.0.0.1:8001',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -165,4 +167,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+AWS_ACCESS_KEY_ID = 'AKIAU2X2GVMVRWV3OR6H'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'moneytrackerbackend'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
