e42c4791138e1369886b6c891837022efd7cd5c3
Owen Zheng <johndoe@example.com>
2024-06-24 19:02:12 -0700

    Verify database settings

--- moneytracker/settings/dev.py
+++ moneytracker/settings/dev.py
@@ -5,18 +5,21 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-20oi6k5$g)j#+pr$saao4xjs1b7l89ki0qiq*p=nee(sn!!3^j'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'moneykeeper',
-        'HOST': 'localhost',
-        'USER': 'root'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'moneykeeper',
+    #     'HOST': 'localhost',
+    #     'USER': 'root'
+    # }
+    'default': dj_database_url.config(
+        default=os.environ.get('JAWSDB_URL')
+    )
 }
 
 AWS_ACCESS_KEY_ID = 'AKIAU2X2GVMVRWV3OR6H'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'moneytrackerbackend'
-AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
--- moneytracker/settings/prod.py
+++ moneytracker/settings/prod.py
@@ -9,17 +9,13 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['moneytracker-prod-9941b1d428c3.herokuapp.com']
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('JAWSDB_URL')
-    )
+    'default': dj_database_url.config()
 }
-
-print(DATABASES)
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'moneytrackerbackend'
-AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_SIGNATURE_NAME = 's3v4',
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
