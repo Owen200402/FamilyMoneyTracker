34415c4654d8a8dbbb8122112457a6ca7fec5f79
Owen Zheng <johndoe@example.com>
2024-06-24 21:33:33 -0700

    Change name for ALLOWED_HOSTS

--- moneytracker/settings/dev.py
+++ moneytracker/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-20oi6k5$g)j#+pr$saao4xjs1b7l89ki0qiq*p=nee(sn!!3^j'
 
 ALLOWED_HOSTS = [
-    "https://moneytracker-prod-c5946e6a479e.herokuapp.com/"
+    "https://moneytracker-prod-c5946e6a479e.herokuapp.com"
 ]
 
 DATABASES = {
